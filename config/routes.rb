f64e1e6686c4449794bbc5e193909cf2cc5e496e
YauheniShakau <zhshakov@gmail.com>
2018-12-03 16:02:49 +0200

    lists, cards, devise User models added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :cards
+  resources :lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "lists#index"
 end
